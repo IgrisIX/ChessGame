968ba9a6e49299618c132eeffe168814164c9acc
IgrisIX <93921275+IgrisIX@users.noreply.github.com>
2023-02-02 13:56:21 +0700

    remove unecessary library

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
-from Classes import *
+from ChessGUI import *
 
-if __name__ == '__main__':
-    gameOver = False
-    game = ChessBoard()
-    gameMode = input("Choose game mode ('pvp' or 'pvc' or 'cvc'): ")
-    if gameMode == "pvp":
-        player1 = input("Player 1 (White) name: ")
-        player2 = input("Player 2 (Black) name: ")
-    turn = 0
-    while not gameOver:
-        if turn % 2 == 0:
-            move = input("Player 1 move: ")
-        elif turn % 2 == 1:
-            move = input("Player 2 move: ")
+app = QApplication(sys.argv)
+window = MainWindow(app)
 
+window.show()
 
-        turn += 1
+app.exec()
 
 
